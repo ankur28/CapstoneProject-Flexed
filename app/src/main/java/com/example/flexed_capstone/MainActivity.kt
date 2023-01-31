efff5609b279d1b166eb3cfc7a991a39609a66f7
Ankur Kalson <ankurkalson28@gmail.com>
2023-01-31 16:13:14 -0500

    main screen for #1

--- app/src/main/java/com/example/flexed_capstone/MainActivity.kt
+++ app/src/main/java/com/example/flexed_capstone/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.flexed_capstone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startBtn : FrameLayout = findViewById(R.id.flStart)
+        startBtn.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Start pressed",Toast.LENGTH_SHORT).show()
+        }
     }
 }
